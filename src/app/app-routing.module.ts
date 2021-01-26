5c5f819c7f9ed57d0bddbd91ab2809ff8da64dc6
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2021-01-26 16:56:19 +0100

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+/**
+ * path: a string that matches the URL in the browser address bar.
+ * component: the component that the router should create when navigating to this route.
+ */
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
