905e4e1d5143550581bb1ef9dbb1b4c1ca0d2e92
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2021-01-26 15:13:23 +0100

    add console functions

--- src/app/services/game-controller.service.ts
+++ src/app/services/game-controller.service.ts
@@ -1,10 +1,44 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {ConsoleService} from './console.service';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
+  /* is provided in any class */
   providedIn: 'root'
 })
 export class GameControllerService {
+  private httpOptions = {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  };
 
-  constructor() {
+  constructor(
+    private http: HttpClient,
+    private console: ConsoleService) {
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T): (error: any) => Observable<T> {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a console message with the ConsoleService */
+  private log(message: string): void {
+    this.console.add(`Game Controller: ${message}`);
   }
 }
